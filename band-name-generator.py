9da503e4f11871751e7199111c570aba5c9f9203
Mário Sacramento <53486744+darwiky@users.noreply.github.com>
2022-12-01 15:58:03 +0000

    Update band-name-generator.py

--- band-name-generator.py
+++ band-name-generator.py
@@ -1,3 +1,5 @@
+# This is a program that asks the user for the name of the city they grew up in and the name of their
+# pet. It then prints out a band name using the city and pet name.
 print("Welcome to the Band Name Generator.")
 street = str(input("What's the name of the city you grew up in?\n"))
 pet = str(input("What's your pet's name?\n"))
